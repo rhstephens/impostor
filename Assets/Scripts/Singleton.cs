adad58c6844c0b4cea1126902e4e63c5486ee5fc
Ryan Stephens <clonetroopa1996@gmail.com>
2017-06-18 23:40:38 -0700

    Initial assets + Singleton script

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base Singleton class. Any class that inherits this will adopt the Singleton design pattern.
+/// </summary>
+/// <typeparam name="T">Type of singleton instance</typeparam>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    protected static T instance;
+
+    // Returns the instance of this singleton
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null) {
+                    Debug.LogError("An instance of " + typeof(T) +
+                       " is needed in the scene, but there is none.");
+                }
+            }
+
+            return instance;
+        }
+    }
+}
