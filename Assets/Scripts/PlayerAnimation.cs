8a81fd0bc49906dd2293b48fce028a7026788d87
Ryan Stephens <clonetroopa1996@gmail.com>
2017-06-30 00:31:32 -0700

    Final touches on anim script

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Animator), typeof(PlayerController))]
+public class PlayerAnimation : NetworkBehaviour {
+
+    Animator anim;
+    PlayerController con;
+
+    [SyncVar(hook = "OnGunDrawn")]
+    bool gunDrawnSync;
+
+    bool draw;
+
+    static string GUN_DRAWN_PARAM = "gunDrawn";
+
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire2")) {
+            DrawGun();
+        }
+    }
+
+    // Any time our server changes the 'drawn' variable, we update the animator
+    void OnGunDrawn(bool drawn) {
+        anim.SetBool(GUN_DRAWN_PARAM, drawn);
+    }
+
+    // Send our player's 'draw' variable to the server
+    void DrawGun() {
+        if (!isLocalPlayer) {
+            return;
+        }
+        draw = !draw;
+        CmdDrawGun(draw);
+    }
+
+    [Command]
+    void CmdDrawGun(bool draw) {
+        gunDrawnSync = draw;
+    }
+}
