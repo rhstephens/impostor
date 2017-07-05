0954d3619d002e79c3c88caf5002c64a77c445fc
Ryan Stephens <clonetroopa1996@gmail.com>
2017-07-04 23:29:14 -0700

    Add basic weapon functionality + hit detection

---
+++ Assets/Scripts/Classes/GameManager.cs
@@ -0,0 +1,12 @@
+﻿
+/// <summary>
+/// The game manager that players will interact with for client-side tasks
+/// </summary>
+public class GameManager : Singleton<GameManager> {
+
+	
+	public IWeapon GetPlayerWeapon() {
+        //TODO: Make this smarter... much smarter
+        return new DesertEagle();
+    }
+}
--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -4,4 +4,39 @@ using UnityEngine.Networking;
 public class PlayerWeapon : NetworkBehaviour {
 
     IWeapon weapon;
+    PlayerController con;
+    RaycastHit2D hit;
+
+    [SerializeField]
+    GameObject gunLocation;
+    [SerializeField]
+    Camera cam;
+
+    void Start() {
+        weapon = GameManager.Instance.GetPlayerWeapon();
+        con = GetComponent<PlayerController>();
+    }
+
+    void Update() {
+        CheckTrigger();
+    }
+
+    void CheckTrigger() {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        if (Input.GetButtonDown("Fire1") && con.gunDrawn) {
+            FireShot();
+        }
+    }
+
+    void FireShot() {
+        hit = Physics2D.Raycast(gunLocation.transform.position, cam.ScreenToWorldPoint(Input.mousePosition), weapon.Range);
+        if (hit) {
+            Debug.Log("Hit something: " + hit.transform.name);
+
+        }
+
+    }
 }
