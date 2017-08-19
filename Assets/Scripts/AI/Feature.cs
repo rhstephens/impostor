1e4c299ba44217d2de627e54e45b3210a31ba0e5
Ryan Stephens <clonetroopa1996@gmail.com>
2017-08-19 00:45:05 -0700

    Initial AI commit. Begin feature generation

---
+++ Assets/Scripts/AI/Feature.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A class that stores and calculates features to be used for the model.
+/// </summary>
+public class Feature : MonoBehaviour {
+
+	// X, Y position on the map
+	public int PosX { get; set; }
+	public int PosY { get; set; }
+
+	// Distance from center
+	public int CenterX { get; set; }
+	public int CenterY { get; set; }
+
+	// Distance from nearest Player/AI
+	public int OpponentX { get; set; }
+	public int OpponentY { get; set; }
+
+	// Is AI in motion?
+	public bool InMotion { get; set; }
+
+	// Time in ms while stationary/in motion
+	public int TimeSinceStartOrStop { get; set; }
+
+	// Time since last direction change
+	public int TimeSinceLastDirection { get; set; }
+
+	public static Feature GeneratePlayerFeatures(GameObject go) {
+		Feature f = new Feature ();
+
+
+		return f;
+	}
+}
---
+++ Assets/Scripts/AI/FeatureMap.cs
@@ -0,0 +1,9 @@
+﻿using CsvHelper.Configuration;
+
+public class FeatureMap : CsvClassMap<Feature> {
+
+	public FeatureMap() {
+		Map (m => m.PosX).Index (0);
+		Map (m => m.PosY).Index (1);
+    }
+}
