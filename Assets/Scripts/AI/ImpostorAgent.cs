2d2e963647e8e8056b61db9b0226ff48bdd2d073
Codetroopa <rhstephens96@gmail.com>
2018-05-05 15:47:28 -0400

    Add the first ever Brain! IT WORKSgit add Assets/! Also increase logic for ImpostorAgent to account for spastic input

--- Assets/Scripts/AI/ImpostorAgent.cs
+++ Assets/Scripts/AI/ImpostorAgent.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
 public class ImpostorAgent : Agent {
 
-    public float walkSpeed = 3f;
+    public float walkSpeed = 1.5f;
+    public float minTime = 0.4f;
 
     Brain studentBrain;
     RayPerception2D rayComponent;
@@ -12,6 +14,7 @@ public class ImpostorAgent : Agent {
     Vector3 velocity;
     PlayerController con;
 
+    float counter = 0f;
 
     // Essentially Start()
     public override void InitializeAgent() {
@@ -32,6 +35,7 @@ public class ImpostorAgent : Agent {
     }
 
     public void Update() {
+        counter += Time.deltaTime;
         velocity.x = direction.x * walkSpeed;
         velocity.y = direction.y * walkSpeed;
 
@@ -48,7 +52,7 @@ public class ImpostorAgent : Agent {
     // This method gets called by the Brain to collect a list of floating point numbers.
     // These numbers are added to the Observations and used by the Brain to make a Decision
     public override void CollectObservations() {
-        float rayDistance = 50f;
+        float rayDistance = 55f;
         float[] rayAngles = { 20f, 70f, 90f, 110f, 160f, 45f, 135f, -20f, -70f, -90f, -110f, -160f, -45f, -135f };
         string[] detectableObjects = { "Player", "Unpassable" };
         AddVectorObs(rayComponent.Perceive(10f, rayAngles, detectableObjects));
@@ -57,8 +61,17 @@ public class ImpostorAgent : Agent {
     // Upon making a Decision, the Brain calls this method with the appropriate action Vector
     public override void AgentAction(float[] act, string textAction) {
         // clamp returned action to nearest direction value
-        direction.x = Mathf.RoundToInt(Mathf.Clamp(act[0], -1f, 1f));
-        direction.y = Mathf.RoundToInt(Mathf.Clamp(act[1], -1f, 1f));
+        int xDir = Mathf.RoundToInt(Mathf.Clamp(act[0], -1f, 1f));
+        int yDir = Mathf.RoundToInt(Mathf.Clamp(act[1], -1f, 1f));
+
+        // In order to prevent "spastic" movements, we need to hold the previously set direction for some amount of time
+        //    before changing it. It works best if this "set amount of time" is randomly determined within an interval
+        if (counter >= minTime) {
+            direction.x = xDir;
+            direction.y = yDir;
+            counter = 0;
+            minTime = UnityEngine.Random.Range(0.2f, 0.75f);
+        }
 
         AddReward(1f);
     }
